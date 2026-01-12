42e44b58eb67c7746727f07a45a7dd4076f76d43
maxitect <maxime.downe@gmail.com>
2026-01-12 22:28:39 +0800

    cd(docker): docker setup for cloudflare tunnel)

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**', // Allow all HTTPS images
+        protocol: "https",
+        hostname: "**", // Allow all HTTPS images
       },
     ],
   },
